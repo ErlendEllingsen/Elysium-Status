5556d159516af449ee73c72c7c44e41a623f734a
Erlend Ellingsen <erlend.ame@gmail.com>
2017-01-12 19:09:35 +0100

    Update

--- static/app.js
+++ static/app.js
@@ -87,7 +87,8 @@ es.render = function() {
             if (foundSrv == null) continue; //Did not found server.
 
             //Check timing of data...
-            var timingOK = ((Math.abs(es.queueData.export_time - foundSrv.last_updated) / 1000) > (3*60)) foundSrv.queueAvailable = false;
+            var timingOK = ((Math.abs(es.queueData.export_time - foundSrv.last_updated) / 1000) > (3*60));
+            if (!timingOK) foundSrv.queueAvailable = false;
 
             var queueText = (foundSrv.queueAvailable ? "Queue: " + foundSrv.queue : 'Queue unavailable');
             $("tr[data-srv='" + name + "']").find('.queueText').html(queueText);
